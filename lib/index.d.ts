f86fb3f45c21203ec9464dea5f91bbd638a11a17
avaxwzy <85783328+AvaxWzy@users.noreply.github.com>
2021-07-02 04:51:13 +0600

    Typings

---
+++ lib/index.d.ts
@@ -0,0 +1,10 @@
+declare module "passcord" {
+
+    export default class Passcord {
+        private number: boolean;
+
+        constructor();
+
+        generate(length: number): void;
+    };
+};
